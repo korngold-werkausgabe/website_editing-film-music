39793045f29ccc9ec3904c3af7c54b8f5a3a4f65
Silke Reich <s.reich@em.uni-frankfurt.de>
2024-10-29 11:48:50 +0100

    nuxt-pdf-frame removed from modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/i18n', 'usebootstrap', "nuxt-security", '@nuxt/scripts', '@i2d/nuxt-pdf-frame'],
+  modules: ['@nuxtjs/i18n', 'usebootstrap', "nuxt-security", '@nuxt/scripts'],
   ssr: true,
   app: {
     head: {
