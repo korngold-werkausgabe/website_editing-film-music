33c477f208b888caf497bcc5c6d708f8b15178a3
Silke Reich <s.reich@em.uni-frankfurt.de>
2024-11-13 14:38:26 +0100

    Links added to markdown formatter

--- plugins/markdownFormatter.ts
+++ plugins/markdownFormatter.ts
@@ -5,7 +5,7 @@ export default defineNuxtPlugin(() => {
             formatMarkdown: (text: string) => {
                 text = text.replace(/\*\*([^\*]+)\*\*/g, '<em>$1</em>');
                 text = text.replace(/_([^_]+)_/g, '<b>$1</b>');
-
+                text = text.replace(/!\[(.*)\]\((.*)\)/g, '<a href="$2" target="_blank">$1</a>');
                 return text;
             }
         }
